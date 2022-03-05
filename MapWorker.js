74ee515845b5453481731bfd82cb88ed3b910499
hunter3789 <84463754+hunter3789@users.noreply.github.com>
2022-03-05 22:24:34 +0900

    Add files via upload

---
+++ MapWorker.js
@@ -0,0 +1,21 @@
+    var url = "https://raw.githubusercontent.com/hunter3789/geojson/main/custom.geo.50m.json";
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.timeout = 60000;
+    xhr.overrideMimeType("application/json; charset=utf-8");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState != 4 || xhr.status != 200) return;
+      else  {
+        var data = JSON.parse(xhr.responseText);
+        postMessage(data);
+        //console.log(json);
+
+
+        //setTimeout(fnGetBounds, 1); 
+
+        //fnGetBounds();
+        //L.geoJson(json).addTo(map);
+        //fnGeoTile(json);
+      }
+    };
+    xhr.send();
